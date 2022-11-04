481e867f8e94696c2f8026befd0477999ef71145
Driner Tucto <1304745@senati.pe>
2022-11-04 18:48:41 -0500

    avance2

--- resources/views/historia.blade.php
+++ resources/views/historia.blade.php
@@ -5,22 +5,38 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>drinerTN</title>
-        
+        <style> 
 
-        <style>
-           header{
-        background-image: url("https://static.vecteezy.com/system/resources/thumbnails/007/278/160/small/polygon-geometric-design-abstract-technology-concept-abstract-blue-light-background-vector.jpg");
-        
-        }
-           
             nav{
                 text-align: center;
             }
             .a{                
                 margin: 20px;
-                margin-top:20px;
+                        
+          margin-top:20px;
 
             }
+            img{
+                width: 280px;
+            height: 280px;
+            }
+            .hola{
+                display: inline-flex;
+                margin-top: 20px;
+
+
+            }
+            h1{
+                margin-right: 1600px;
+                text-align: center;
+            }
+            p{
+                margin-left: 20px;
+            }
+            footer{
+                text-align: center;
+            }
+
 
         </style>
 
@@ -31,17 +47,49 @@
         <a class ="a" href="CONTACTAME">CONTACTAME</a>     
     </nav>
       </header>
-    
-      <h1>Driner</h1>
+      <h1>Machu Picchu</h1>
 
+    <div class="hola">
+    <img src="https://tipsparatuviaje.com/wp-content/uploads/2019/07/machu-picchu.jpg">
+    <p>
+    Esta Entre los principales inmuebles coloniales destacan la Catedral de Lima, la Plaza Mayor, la Universidad de San Marcos (primera de América, fundada en 1551), el Palacio de Torre Tagle, el Palacio Arzobispal y más de 1600 casonas y edificios con balcones, elemento que constituye el principal distintivo arquitectónico de la ciudad.
+
+Con su antigua denominación de Ciudad de los Reyes, fue la capital del enorme Virreinato del Perú entre 1542 y 1821, entidad que abarcaba las posesiones españolas en Suramérica (excepto Venezuela) y algunas islas de Oceanía.ciudadela inca declarada “santuario histórico” es la atracción turística más famosa de Perú y uno de los yacimientos arqueológicos más importantes de América. Está en el Valle Sagrado de los Incas, a 2400 m.s.n.m. y a 75 km de Cusco, la antigua capital imperial prehispánica.</p>
+</div>
+
+<div class="hola">
+    <p>
+    La principal ciudad peruana a orillas del lago es Puno, capital del departamento de igual nombre, con una población mayoritariamente indígena.
+
+Una de las principales actividades puneñas es la pesca en caballitos de totora, hechas con este junco acuático. La totora también es utilizada por los indígenas uros para hacer sus famosas islas artificiales flotantes. Estas islas se fabrican superponiendo varias capas de totora tejida en esteras, sobre las cuales va una sencilla vivienda de una sola habitación.
+
+Las principales actividades turísticas en el Titicaca son pasear en barco y en caballitos de totora, visitar las islas y practicar la pesca deportiva. En el lago hay 42 islas naturales, siendo Amantani, de 9.3 km2, la más grande de Perú.
+
+En las islas lacustres, incluyendo las artificiales, viven comunidades ancestrales que han conservado sus costumbres y rasgos culturales.
+
+La ciudad de Puno es una de las más alegres de Perú y su Fiesta de La Candelaria fue declarada Patrimonio Cultural de la Humanidad. Participan unos 40000 danzantes y cerca de 9000 músicos.</p>   
+    <img src="https://tipsparatuviaje.com/wp-content/uploads/2019/07/lago-titicaca-1.jpg">
+
+</div>
     </body>
 
 
 
+<!-- /* THE CODE TO COPY AND PASTE
+composer install 
+mv .env.example .env 
+php artisan cache:clear 
+composer dump-autoload 
+php artisan key:generate
+
+HOW TO CLONE A LARAVEL PROJECT FROM GITHUB https://youtu.be/k2zzbTFpYgA
+HOW TO UPLOAD LARAVEL PROJECT TO GITHUB FOR BEGINNERS https://youtu.be/1ziIpWrcL00 -->
 
 
+<footer>
 
-
+driner@tucto2022
+</footer>
 
 
 </html>

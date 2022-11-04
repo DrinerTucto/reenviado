29f358e4da597bcea30ef0f89ba89ad88497cf77
Driner Tucto <1304745@senati.pe>
2022-11-04 18:07:06 -0500

    inicio

---
+++ resources/views/historia.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>drinerTN</title>
+        
+
+        <style>
+           header{
+        background-image: url("https://static.vecteezy.com/system/resources/thumbnails/007/278/160/small/polygon-geometric-design-abstract-technology-concept-abstract-blue-light-background-vector.jpg");
+        
+        }
+           
+            nav{
+                text-align: center;
+            }
+            .a{                
+                margin: 20px;
+                margin-top:20px;
+
+            }
+
+        </style>
+
+        <header>
+        <nav>
+        <a class="a" href="INICIO">INCIO</a>     
+        <a class="a" href="INFORMACION">INFORMACION</a>     
+        <a class ="a" href="CONTACTAME">CONTACTAME</a>     
+    </nav>
+      </header>
+    
+      <h1>Driner</h1>
+
+    </body>
+
+
+
+
+
+
+
+
+
+</html>
